9c7bfc79152ee2759825c948ebeeaacb07542792
Linus Eklind <76260367+Eklinden@users.noreply.github.com>
2022-01-28 15:41:07 +0100

    scroll popup

--- portfolio2/index.js
+++ portfolio2/index.js
@@ -19,3 +19,19 @@ window.addEventListener('scroll', () => {
 
 
 })
+
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 250;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      }
+    }
+  }
+  
+  window.addEventListener("scroll", reveal);
